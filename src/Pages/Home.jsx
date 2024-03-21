d69cc5a0028f977fadeee8530587c320a1d9cdfe
iftakhar <iftakharalamshuvo11@gmail.com>
2024-03-21 14:23:18 +0600

    initial file added

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from '../components/Nav/Nav';
+
+const Home = () => {
+    return (
+        <div>
+            <Nav></Nav>
+            <h2>Home page</h2>
+        </div>
+    );
+};
+
+export default Home;
